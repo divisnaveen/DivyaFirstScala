24164014ea6eaca1e4fae0c68ead22aed80e5c32
divisnaveen <divisnaveen@gmail.com>
2016-10-23 01:18:47 -0700

    Initial commit

---
+++ src/main/scala/Kg.scala
@@ -0,0 +1,10 @@
+package Admission
+/**
+  * Created by Lenovo on 10/20/2016.
+  */
+class Kg {
+  def getProduct(x:Int,y:Int)=x*y
+
+
+
+}
---
+++ src/test/scala-2.11/Admission/KgTest.scala
@@ -0,0 +1,17 @@
+package Admission
+
+import org.scalatest.FunSuite
+
+/**
+  * Created by Lenovo on 10/21/2016.
+  */
+class KgTest extends FunSuite {
+
+  test("testGetProduct") {
+    val product = 5+9
+    assert(product == 14)
+
+
+  }
+
+}
